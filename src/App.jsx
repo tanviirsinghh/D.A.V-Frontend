abfefe3d274ad56c44aad80e545f256d02cfe36c
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-11-09 11:37:54 +0530

    moved vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-
 import './App.css'
 import Login from './Authentication/Login'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from './Pages/LandingPage';
-import Otp from './Authentication/Otp';
-import RegistrationPage from './Authentication/registration';
-import MedicineProductListing from './Pages/product';
-import UserListing from './Pages/userlisting';
-import TrackOrder from './Pages/trackOrder';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LandingPage from './Pages/LandingPage'
+import Otp from './Authentication/Otp'
+import RegistrationPage from './Authentication/registration'
+import MedicineProductListing from './Pages/product'
+import UserListing from './Pages/userlisting'
+import TrackOrder from './Pages/trackOrder'
 
+import UserApprovalList from './Pages/approval'
+import OrderHistory from './Pages/History/outer'
+import PerticularHistory from './Pages/History/inner'
 
-import UserApprovalList from './Pages/approval';
-import OrderHistory from './Pages/History/outer';
-import PerticularHistory from './Pages/History/inner';
+import AddProduct from './Pages/AddProduct'
+import GenerateOrder from './Pages/GenerateOrder'
+import UserInfo from './Pages/UserInfo'
+import RequestProduct from './component/RequestProduct'
+import RequestSmgri from './component/RequestSmgri'
 
-import AddProduct from './Pages/AddProduct';
-import GenerateOrder from './Pages/GenerateOrder';
-import UserInfo from './Pages/UserInfo';
-import RequestProduct from './component/RequestProduct';
-import RequestSmgri from './component/RequestSmgri';
+import AyurvedicMedicineOrders from './Pages/orderList'
+import GenerateProduct from './Pages/generateProduct'
+import RequestRaw from './Pages/rawRequest'
 
-
-import AyurvedicMedicineOrders from './Pages/orderList';
-import GenerateProduct from './Pages/generateProduct';
-import RequestRaw from './Pages/rawRequest';
-
-
-
-function App() {
-
+function App () {
   return (
     <>
       <BrowserRouter>
@@ -43,19 +37,22 @@ function App() {
 
           <Route path='/userapprovallist' element={<UserApprovalList />} />
           <Route path='/history' element={<OrderHistory />} />
-          <Route path='/history/perticularhistory' element={<PerticularHistory />} />
+          <Route
+            path='/history/perticularhistory'
+            element={<PerticularHistory />}
+          />
           <Route path='/addproduct' element={<AddProduct />} />
           <Route path='/generateorder' element={<GenerateOrder />} />
           <Route path='/userinfo' element={<UserInfo />} />
           <Route path='/requestProduct' element={<RequestProduct />} />
           <Route path='/requestsmgri' element={<RequestSmgri />} />
 
-
-
-
           <Route path='/userapprovallist' element={<UserApprovalList />} />
           <Route path='/history' element={<OrderHistory />} />
-          <Route path='/history/perticularhistory' element={<PerticularHistory />} />
+          <Route
+            path='/history/perticularhistory'
+            element={<PerticularHistory />}
+          />
 
           <Route path='/addproduct' element={<AddProduct />} />
           <Route path='/generateorder' element={<GenerateOrder />} />
@@ -63,16 +60,8 @@ function App() {
           <Route path='/orderlist' element={<AyurvedicMedicineOrders />} />
           <Route path='/requestproduct' element={<GenerateProduct />} />
           <Route path='/request_raw' element={<RequestRaw />} />
-
-
-
-
-
         </Routes>
-
       </BrowserRouter>
-
-
     </>
   )
 }
